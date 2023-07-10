099eaef4c59e291cc690d5ee75cfd3e6d908b5d6
cjporter25 <chris.j.porter25@gmail.com>
2023-07-09 20:52:32 -0500

    Completed - Minimum Working Page - 7/9/23

--- home/views.py
+++ home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+# The function "home" receives a request
+def home(request):
+    # Returns an HttpResponse containing the string "Hello World"
+    return HttpResponse('Hello, world!')
