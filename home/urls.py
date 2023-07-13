9931e2cced538785bcc9efe8cdc41405bc97ecd6
cjporter25 <chris.j.porter25@gmail.com>
2023-07-13 18:37:03 -0500

    Recommitting

--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     # The 'home' parameter represents the DIR path we are taking to access
     #   the "views.py" file. As such, the parameter "views.home" points to
     #   the "home" FUNCTION inside that file.
-    path('home', views.home)
+    path('home', views.home),
+    path('authorized', views.authorized)
 ]
