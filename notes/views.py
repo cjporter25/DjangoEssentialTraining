9e0982b5b49c1462b8e076adffdcc7f69517a0be
cjporter25 <chris.j.porter25@gmail.com>
2023-07-24 23:15:31 -0500

    Clean-up

--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 
 # This import statement specifies that we are importing the 
 #   class "Notes" from the python file "models" in the same directory
@@ -6,7 +7,7 @@ from .models import Notes
 
 # This file is where "views" or "templates" are created for an app
 
-# Function - list()
+# Function - list(req)
 # Usage - Take in a request and return all note objects fromn the database
 def list(request):
     # Create a variable and save a call to all notes objects
@@ -17,3 +18,19 @@ def list(request):
     # NOTE: Remember that the target HTML file must include the same variable name so that
     #       it can actually receive the input
     return render(request, 'notes/notes_list.html', {'notes': all_notes})
+
+# Function - detail(req, pk)
+# Usage - Retrieve a specific note object from the database based on an 
+#         input pk (private key)
+def detail(request, pk):
+    # Try retrieving the object
+    try:
+        note = Notes.objects.get(pk=pk)
+    # If the specified object doesn't exist, raise an Http404 error.
+    except Notes.DoesNotExist:
+        raise Http404("Note doesn't exist")
+        # Return a render of a different template, but pass in the local "note" 
+        #   object created above
+    return render(request, 'notes/notes_detail.html', {'note': note})
+    # NOTE: Because we are passing in another parameter, the urlpattern that points to this 
+    #       function will need to account for that.
