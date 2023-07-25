d3adcc897ab3d7bd1f521be40ccf0d7941f4fadb
cjporter25 <chris.j.porter25@gmail.com>
2023-07-24 23:15:29 -0500

    Completed single note content - 7/24/23

--- notes/urls.py
+++ notes/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     # The current path indicates we have a function called "list" in views that 
     #   should be activated.
     path('notes', views.list), 
+    # This URL will receive a new value called "pk" with type "int" to indicate that 
+    #   this value can contain a variety of 
+    path('notes/<int:pk>', views.detail)
+
 ]
