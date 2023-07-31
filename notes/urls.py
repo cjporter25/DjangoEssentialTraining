6126f6aa5c9dc095570ae61ee724b66288dc8319
cjporter25 <chris.j.porter25@gmail.com>
2023-07-30 21:01:32 -0500

    Clean-up Commit

--- notes/urls.py
+++ notes/urls.py
@@ -5,11 +5,13 @@ from . import views
 
 # Create a urlpatterns object to establish URL paths for notes -->
 urlpatterns = [
+    path('notes', views.NotesListView.as_view()), # Class based implementation
+
     # The current path indicates we have a function called "list" in views that 
     #   should be activated.
-    path('notes', views.list), 
+    #           path('notes', views.list), 
     # This URL will receive a new value called "pk" with type "int" to indicate that 
     #   this value can contain a variety of 
-    path('notes/<int:pk>', views.detail)
+    path('notes/<int:pk>', views.NotesDetailView.as_view())
 
 ]
